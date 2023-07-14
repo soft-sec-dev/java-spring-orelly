3cde1016ded488671c8046864a7f7522649de500
jordanhuaman <jordanelcracken@gmail.com>
2023-07-14 16:32:18 -0500

    feat: initial commit on chap-3

---
+++ chap-3/src/main/java/jordan/java/chap3/Chap3Application.java
@@ -0,0 +1,13 @@
+package jordan.java.chap3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chap3Application.class, args);
+	}
+
+}
