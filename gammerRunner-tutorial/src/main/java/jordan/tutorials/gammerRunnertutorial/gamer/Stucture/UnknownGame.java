0da9e24cf606644e0655eae54fdb3a5ec5c39514
jordanhuaman <jordanelcracken@gmail.com>
2023-07-13 23:05:27 -0500

    Add: new Game

---
+++ gammerRunner-tutorial/src/main/java/jordan/tutorials/gammerRunnertutorial/gamer/Stucture/UnknownGame.java
@@ -0,0 +1,26 @@
+package jordan.tutorials.gammerRunnertutorial.gamer.Stucture;
+
+import jordan.tutorials.gammerRunnertutorial.gamer.interfaces.GammingConsole;
+
+public class UnknownGame implements GammingConsole {
+
+    @Override
+    public void up() {
+        System.out.println("Unknow up");
+    }
+
+    @Override
+    public void down() {
+        System.out.println("Unnow down");
+    }
+
+    @Override
+    public void left() {
+        System.out.println("Unknow left");
+    }
+
+    @Override
+    public void right() {
+        System.out.println("Unnow right");
+    }
+}
